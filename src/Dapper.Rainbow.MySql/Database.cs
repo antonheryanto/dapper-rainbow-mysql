33384461dd5e96fb61b80f6b8200adf7f4f367ea
anton heryanto <anton.heryanto@gmail.com>
2012-08-29 00:16:00 +0800

    fix table name

--- src/Dapper.Rainbow.MySql/Database.cs
+++ src/Dapper.Rainbow.MySql/Database.cs
@@ -110,13 +110,25 @@ namespace Dapper
                 string cols_update = string.Join(",", paramNames.Select(p => "`" + p + "` = @" + p));                
                 string key_update = "`" + k + "` = LAST_INSERT_ID(`" + k + "`)";
                 var b = new StringBuilder();
-                b.Append("INSERT INTO `").Append(tableName).Append("` (`").Append(cols).Append("`,`").Append(k).Append("`) VALUES (")
+                b.Append("INSERT INTO `").Append(TableName).Append("` (`").Append(cols).Append("`,`").Append(k).Append("`) VALUES (")
                  .Append(cols_params).Append(", @").Append(k)
                  .Append(") ON DUPLICATE KEY UPDATE ").Append("`").Append(k).Append("` = LAST_INSERT_ID(`").Append(k).Append("`)")
                  .Append(", ").Append(cols_update).Append(";SELECT LAST_INSERT_ID()");                
                 var parameters = new DynamicParameters(data);
                 parameters.AddDynamicParams(key);
                 return database.Query<long>(b.ToString(), parameters).Single();
+            }
+
+            public int InsertOrUpdate(dynamic data)
+            {
+                List<string> paramNames = GetParamNames((object)data);
+                string cols = string.Join("`,`", paramNames);
+                string cols_params = string.Join(",", paramNames.Select(p => "@" + p));
+                string cols_update = string.Join(",", paramNames.Select(p => "`" + p + "` = @" + p));                
+                var b = new StringBuilder();
+                b.Append("INSERT INTO `").Append(tableName).Append("` (`").Append(cols).Append("`) VALUES (")
+                 .Append(cols_params).Append(") ON DUPLICATE KEY UPDATE ").Append(cols_update);
+                return database.Execute(b.ToString(), data);
             }
 
             /// <summary>
